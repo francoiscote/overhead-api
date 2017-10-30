4a733d6ae810a2946bb1bcc8f816145a0cd0f815
François Côté <francois@francoiscote.net>
2017-10-29 22:22:22 -0400

    socket refactor

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
-const app = require('express')();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-
-const fetch = require('isomorphic-fetch');
+const debug = require('debug')('overhead-api');
+const axios = require('axios');
+const socketio = require('socket.io');
 
 const PORT = process.env.PORT || 3001;
+const watcherInterval = 5000;
+const radiusKm = 2;
 
-const latitude = 45.4697;
-const longitude = -73.7449;
-const radiusKm = 10;
-const url = `https://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?lat=${latitude}8&lng=${longitude}&fDstL=0&fDstU=${radiusKm}`;
-
-setInterval(() => {
-  fetch(url)
-    .then(
-      (response) => {
-        if (!response.ok) {
-          throw Error('Could not fetch data');
-        }
-        return response.json();
-      },
-      error => console.log('Fetch Error occured:', error),
-    )
-    .then((json) => {
-      console.log('emit planes');
-      io.emit('planes', json);
-    });
-}, 5 * 1000);
-
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+const io = socketio(PORT);
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  debug(`Client Connected: ID ${socket.id}`);
 
-  io.emit('message', 'hello user');
+  const { latitude, longitude } = socket.handshake.query;
+  debug(`LAT/LONG for ID ${socket.id}: \n  LAT:${latitude}\n  LONG:${longitude}`);
+
+  // Start a Watcher for this client
+  const url = `https://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?lat=${latitude}&lng=${longitude}&fDstL=0&fDstU=${radiusKm}`;
+
+  const timeout = setInterval(() => {
+    debug(`---FETCHING PLANES for client: ${socket.id} ---`);
+    axios.get(url)
+      .then((response) => {
+        if (response.statusText !== 'OK') {
+          throw Error('Could not fetch data');
+        }
+        return response.data;
+      })
+      .then((planes) => {
+        // TODO: use normalizer to restructure the data for react/redux usage.
+        if (planes.acList.length) {
+          debug(`---EMIT PLANES for client: ${socket.id} ---`);
+          socket.emit('planes.update', planes);
+        } else {
+          debug(`---NO PLANES for client: ${socket.id} ---`);
+        }
+      })
+      .catch((error) => {
+        debug('Fetch Error occured:', error);
+      });
+  }, watcherInterval);
 
   socket.on('disconnect', () => {
-    console.log('a user disconnected');
+    clearInterval(timeout);
+    debug(`Client Disconnected: ID ${socket.id}`);
   });
-});
-
-server.listen(PORT, () => {
-  console.log(`Listening on *:${PORT}`);
 });
